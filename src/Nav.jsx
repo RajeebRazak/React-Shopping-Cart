4537a1b4e8c02684120161ac0f4b4f5e40071deb
RajeebRazak <rajeebrazak786@gmail.com>
2023-12-23 12:31:59 +0530

    update

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -96,8 +96,8 @@ function Nav({addcart, handleRemove, totalPrice}) {
             <ol>
               {addcart.map((item, index) => {
                 return <>
-                <div className='cartlist'>
-                  <li key={index}>{item.headName}</li>
+                <div className='cartlist'  key={index}>
+                  <li>{item.headName}</li>
                   <div >${(item.discountedPrice)? item.discountedPrice : item.singlePrice}</div>
                   <button className='btn btn-danger btn-sm' onClick={() => {handleRemove(item)}}>X</button>
                 </div>
